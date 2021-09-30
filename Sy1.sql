2102748368e14396e9da5a40a159ed602b81483a
Thiago Radunz <86537213+ThiagoRadunz@users.noreply.github.com>
2021-09-30 11:32:14 -0300

    Delete Sy1.sql

--- Sy1.sql
+++
@@ -1,9 +0,0 @@
-﻿                                    SELECT PRODUTO.ID IDProduto,
-                                           PRODUTO.NOME Nome,
-                                           CATEGORIA.NOME Categoria,
-                                           PRODUTO.PRECO_UNITARIO PrecoUnitario,
-                                           PRODUTO.QTD_ESTOQUE QtdEstoque,
-                                           PRODUTO.DESCRICAO Descricao
-                                      FROM PRODUTOS PRODUTO 
-                                      JOIN CATEGORIAS CATEGORIA
-                                        ON PRODUTO.CATEGORIAID = CATEGORIA.ID
